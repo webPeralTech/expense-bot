52df361edaaa448c7519ca98e21bf1cb515f2aaa
webPeralTech <jackofficial388@gmail.com>
2025-07-30 17:50:42 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.PROD ? '/api' : 'https://expense-bot-3g8s.onrender.com/api' /* 'http://localhost:5000/api' */
+const API_BASE_URL = /* import.meta.env.PROD ? '/api' : */ 'https://expense-bot-3g8s.onrender.com/api' /* 'http://localhost:5000/api' */
 
 const api = axios.create({
   baseURL: API_BASE_URL,
